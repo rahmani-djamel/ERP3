55cf867697e0f188b20328030a191249f21b51c0
Nada-Iftene <nadaiftene0311@gmail.com>
2023-09-21 23:32:16 +0200

    new modif

--- resources/views/livewire/backend/company/employee/index.blade.php
+++ resources/views/livewire/backend/company/employee/index.blade.php
@@ -1,6 +1,9 @@
-
-<div>
-    <h1>
-        write your code here
-    </h1>
+<div id="main-content" class="relative w-full h-full  overflow-y-auto bg-gray-50 rtl:lg:mr-64 ltr:lg:ml-64 dark:bg-gray-900">
+    <main>
+        <div>
+            <h1>
+                write your code here
+            </h1>
+        </div>
+    </main>
 </div>
