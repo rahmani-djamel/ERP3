fbddc588904f9f5325dd64724f2bc9e2ff7a911c
rahmani djamel <hxhdono11@gmail.com>
2023-10-17 18:48:05 +0200

    new fixes

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -17,6 +17,8 @@
         <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
 
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+
         @livewireStyles
         @livewireScripts
 
