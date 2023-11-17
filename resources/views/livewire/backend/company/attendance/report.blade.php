a8ec73030755ddc0b7310f90ae6b7e1abe6562cd
rahmani djamel <hxhdono11@gmail.com>
2023-11-17 16:37:02 +0100

    new fixing

--- resources/views/livewire/backend/company/attendance/report.blade.php
+++ resources/views/livewire/backend/company/attendance/report.blade.php
@@ -70,8 +70,8 @@
                                                                 <x-native-select
                                                                 label=""
                                                                 class="
-                                                                    {{ $val->status == __('Present') ? 'bg-green-400 text-white' : '' }}
-                                                                    {{ $val->status == __('Absent') ? 'bg-cyan-700 text-white' : '' }}
+                                                                    {{ $val->status == __('Present') ? '!bg-green-400 text-white' : '' }}
+                                                                    {{ $val->status == __('Absent') ? '!bg-cyan-700 text-white' : '' }}
                                                                     {{ $val->status == __('Vacance') ? 'bg-indigo-900 text-white' : '' }}
                                                                     {{ $val->status ==  __('not specify') ? 'bg-yellow-950 text-white' : '' }}
                                                                 "
